c842f3e87c113396f9bf2acdf331d057d5747cb6
Simon <simontom@volny.cz>
2019-05-26 15:42:47 +0200

    Fixed indenting

--- SharedCodeFiles/Radio.h
+++ SharedCodeFiles/Radio.h
@@ -33,32 +33,32 @@ typedef void(*DataProcessor)(bool isReceivedSuccessfully, uint8_t from, uint8_t 
 class Radio {
 
 	public:
-	Radio(uint8_t address, uint8_t slaveSelectPin = SLAVE_SELECT_PIN, uint8_t interruptPin = INTERRUPT_PIN);
-	void init(LEDutilities &led);
+		Radio(uint8_t address, uint8_t slaveSelectPin = SLAVE_SELECT_PIN, uint8_t interruptPin = INTERRUPT_PIN);
+		void init(LEDutilities &led);
 
-	void maintainRouting(void);
-	void receiveWeatherDataAndProcess(WeatherData &dataContainer, WeatherDataProcessor dataProcessor);
-	void receiveDataAndProcessWithTimeout(uint8_t *buffer, uint8_t len, uint16_t timeout, DataProcessor dataProcessor);
+		void maintainRouting(void);
+		void receiveWeatherDataAndProcess(WeatherData &dataContainer, WeatherDataProcessor dataProcessor);
+		void receiveDataAndProcessWithTimeout(uint8_t *buffer, uint8_t len, uint16_t timeout, DataProcessor dataProcessor);
 
-	bool sendWeatherData(uint8_t destinationAddress, WeatherData &data, LEDutilities *led = nullptr);
-	bool sendData(uint8_t destinationAddress, uint8_t * data, uint8_t length, LEDutilities *led = nullptr);
+		bool sendWeatherData(uint8_t destinationAddress, WeatherData &data, LEDutilities *led = nullptr);
+		bool sendData(uint8_t destinationAddress, uint8_t * data, uint8_t length, LEDutilities *led = nullptr);
 
-	inline void sleep(void) const { driver->sleep(); }
+		inline void sleep(void) const { driver->sleep(); }
 
-	void printRoutingTable(void);
-	int16_t getLastRssi(void);
+		void printRoutingTable(void);
+		int16_t getLastRssi(void);
 
 	private:
-	RH_RF22 *driver;
-	RHMesh *manager;
+		RH_RF22 *driver;
+		RHMesh *manager;
 
-	uint8_t buff[20];
-	const uint8_t buffLen = 20;
-	uint8_t from = 0xFF;
+		uint8_t buff[20];
+		const uint8_t buffLen = 20;
+		uint8_t from = 0xFF;
 
-	#if WATCHDOG_ENABLED
-	uint8_t tx_errors_counter = 0;
-	#endif
+		#if WATCHDOG_ENABLED
+			uint8_t tx_errors_counter = 0;
+		#endif
 
 };
 
